930c23b90ddbdc7931bed2deac64e88a1ebe5c79
Luiz Alves <luizalvestreze@gmail.com>
2025-02-18 17:59:48 -0500

    Creation the list with the API data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const url = "https://pokeapi.co/api/v2/pokemon/";
-const header = {"constenttype": "aplication/json"};
+const url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+const header = {"Content-type": "application/json"};
 
 fetch( url, {
     method: "GET",
@@ -7,13 +7,21 @@ fetch( url, {
 })
 
 .then(response => {
-    if (!response>ok) {
+    if (!response.ok) {
         throw new Error("Erro na requisição" + response.status);
     }
     return response.json();
 })
 .then(data => {
-    console.log("Dados recebidos:", data);
+    console.log(data);
+    const pokemon = document.getElementById("pokemon");
+    pokemon.innerHTML = "";
+
+    data.results.forEach(pokemon => {
+        const pokeList = document.createElement("p");
+        pokeList.textContent = pokemon.name;
+        pokemon.appendChild(pokeList);
+    });
 })
 .catch(error => {
     console.error("Erro:", error);
